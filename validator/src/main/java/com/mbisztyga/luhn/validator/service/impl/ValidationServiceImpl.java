98cf60efb8613bc235467a5c79baa426d56eeff4
mbisztyga <m.bisztyga777@gmail.com>
2023-10-08 02:34:46 +0200

    All logic working correctly

--- validator/src/main/java/com/mbisztyga/luhn/validator/service/impl/ValidationServiceImpl.java
+++ validator/src/main/java/com/mbisztyga/luhn/validator/service/impl/ValidationServiceImpl.java
@@ -11,6 +11,36 @@ public class ValidationServiceImpl implements ValidationService {
     }
 
     private String validateUsingLuhn(String receivedToken) {
-        return receivedToken+"string message about validness";
+
+        String tokenWithoutDashes = receivedToken.replaceAll("-", "");
+
+        char[] digits = tokenWithoutDashes.toCharArray();
+
+        int[] reversedDigits = new int[16];
+
+        for (int i = 0; i < 16; i++) {
+            reversedDigits[i] = Character.getNumericValue(digits[15 - i]);
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < 16; i++) {
+            int digit = reversedDigits[i];
+            
+            if (i % 2 == 1) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit -= 9;
+                }
+            }
+            sum += digit;
+        }
+
+        if (sum % 10 == 0) {
+            return receivedToken+" is a valid token";
+        }
+        else {
+            return receivedToken+" is an invalid token";
+        }
     }
 }
